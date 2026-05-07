5c9238a81724ae09bb21df579bf4b15cfbda0a5f
Austin Nathaniel Miranda <96866938+TarokoDev@users.noreply.github.com>
2026-05-07 12:56:40 +0800

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable1 = 12
+print(type(variable1))
+variable2 = 12.3
+print(type(variable2))
+variable3 = True
+print(type(variable3))
+variable4 = "False"
+print(type(variable4))
+variable5 = ["True"]
+print(type(variable5))
