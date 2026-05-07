572a158d8d9e1bb2b0cff2bde4d68843723cb349
Austin Nathaniel Miranda <96866938+TarokoDev@users.noreply.github.com>
2026-05-07 12:54:23 +0800

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 12
+float_type = 12.99
+bool_type = True
+string_type = "True"
+list_type = ["True", "False"]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
