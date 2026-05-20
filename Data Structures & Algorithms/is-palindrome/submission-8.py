179683de89cc950a32b19efad44e91c2c59875fd
Austin Nathaniel Miranda <96866938+TarokoDev@users.noreply.github.com>
2026-05-20 23:32:31 +0800

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower();
+
+        left = 0
+        right = len(s) - 1
+
+        while left <= right:
+            if s[left].isalnum() != True:
+                left += 1
+            if s[right].isalnum() != True:
+                right -= 1
+
+            if left <= right:
+                if s[left] != s[right]:
+                    return False
+                else:
+                    left += 1
+                    right -= 1
+
+        return True
