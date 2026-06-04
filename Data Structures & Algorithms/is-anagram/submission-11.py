50f9644822646d7c35fa4d0edfd1f933a09349d3
Austin Nathaniel Miranda <96866938+TarokoDev@users.noreply.github.com>
2026-06-04 11:25:51 +0800

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if (len(s) != len(t)):
+            return False
+
+        s_map = {}
+        t_map = {}
+
+        for char in s:
+            if char in s_map:
+                s_map[char] += 1
+            else:
+                s_map[char] = 1
+
+        for char in t:
+            if char in t_map:
+                t_map[char] += 1
+            else:
+                t_map[char] = 1
+
+        if t_map == s_map:
+            return True
+        else:
+            return False
+        
